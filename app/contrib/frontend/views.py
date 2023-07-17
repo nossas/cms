b594f392060b64e81849c96df03a02256ee352b9
miguelzinh3 <miguelssoares@pm.me>
2023-07-17 17:05:48 -0300

    feat(errors): add error handle for 404 and 500

--- app/contrib/frontend/views.py
+++ app/contrib/frontend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def error_404(request, exception):
+def error_404(request, exception=None):
     return render(request, "errors/404.html", status=404)
 
 def error_500(request, exception=None):
