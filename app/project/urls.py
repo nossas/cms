28168a449fcf28b2d7d25677706ca1ee953f6d86
miguelzinh3 <miguelssoares@pm.me>
2023-07-17 02:10:11 -0300

    feat: add 404 and 500 pages on views and urls.py

--- app/project/urls.py
+++ app/project/urls.py
@@ -29,8 +29,10 @@ urlpatterns = [
 
 urlpatterns += staticfiles_urlpatterns()
 
+handler404 = 'contrib.frontend.views.error_404'
+handler500 = 'contrib.frontend.views.error_500'
+
 if settings.DEBUG:
     urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
-
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
