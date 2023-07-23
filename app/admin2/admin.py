24190a57efff96f83ed824fbf72d0f79775956b1
Igor Santos <igr.exe@gmail.com>
2023-07-23 11:25:46 -0300

    feat(admin2): add styled to delete and history admin model

--- app/admin2/admin.py
+++ app/admin2/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 class ModelAdmin(admin.ModelAdmin):
     change_list_template = "admin2/change_list.html"
     change_form_template = "admin2/change_form.html"
+    object_history_template = "admin2/object_history.html"
+    delete_confirmation_template = "admin2/delete_confirmation.html"
+    delete_selected_confirmation_template = "admin2/delete_selected_confirmation.html"
