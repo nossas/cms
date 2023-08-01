bb3f791e9e556f7f815aad320f777fcbbb609ea3
Igor Santos <igr.exe@gmail.com>
2023-08-01 10:38:05 -0300

    feat(eleicao): add filter by state

--- app/eleicao/views.py
+++ app/eleicao/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
@@ -10,12 +12,19 @@ class CandidateView(ListView):
     template_name = "eleicao/candidate_list.html"
     model = Candidate
 
+    def get_queryset(self) -> QuerySet[Any]:
+        filter_state = self.request.GET.get("uf", None)
+        if filter_state:
+            return Candidate.objects.filter(state__iexact=filter_state)
+
+        return super().get_queryset()
+
 
 class CandidateCreateView(CreateView):
     template_name = "eleicao/candidate_form.html"
     model = Candidate
     fields = "__all__"
-    
+
 
 class CandidateDetailView(DetailView):
     template_name = "eleicao/candidate_detail.html"
