3161fed51fe9182d4c99a612f151361e447f5528
Igor Santos <igr.exe@gmail.com>
2023-08-02 15:52:25 -0300

    fix(eleicao): change urls path

--- app/eleicao/urls.py
+++ app/eleicao/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import CandidateView, CandidateCreateView, CandidateDetailView, VoterCreateView
+from .views import CandidateListView, CandidateCreateView, CandidateDetailView, VoterCreateView
 from .places.views import fetch_cep
 
 urlpatterns = [
-    path("candidatas/", CandidateView.as_view(), name="candidate_list"),
-    path("candidatas/criar/", CandidateCreateView.as_view(), name="candidate_create"),
-    path("candidatas/<slug:slug>/", CandidateDetailView.as_view(), name="candidate_detail"),
+    path("candidaturas/", CandidateListView.as_view(), name="candidate_list"),
+    path("candidaturas/cadastro/", CandidateCreateView.as_view(), name="candidate_create"),
+    path("<slug:slug>/", CandidateDetailView.as_view(), name="candidate_detail"),
     # Filtro de endereço
     path("cep/", fetch_cep, name="cep"),
-    path("querovotar/descubra/", VoterCreateView.as_view(), name="voter_match"),
+    path("querovotar/", VoterCreateView.as_view(), name="voter_match"),
 ]
