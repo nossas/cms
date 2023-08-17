9649d7ae06b64458974acbae2ef4525f7ab53504
Igor Santos <igr.exe@gmail.com>
2023-08-17 14:12:03 -0300

    feat(eleicao): add candidate list like a cms plugin

--- app/eleicao/cms_plugins.py
+++ app/eleicao/cms_plugins.py
@@ -2,6 +2,9 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
+from .models import Candidate, CandidateStatusChoices
+from .forms.filters import CandidateListFilter
+
 
 @plugin_pool.register_plugin
 class EleicaoNavbarPlugin(CMSPluginBase):
@@ -15,3 +18,28 @@ class EleicaoFooterPlugin(CMSPluginBase):
     name = "Assinatura"
     module = "A Eleição do Ano"
     render_template = "eleicao/plugins/footer.html"
+
+
+@plugin_pool.register_plugin
+class EleicaoCandidateListPlugin(CMSPluginBase):
+    name = "Lista de candidaturas"
+    module = "A Eleição do Ano"
+    render_template = "eleicao/plugins/candidate_list.html"
+
+    def render(self, context, instance, placeholder):
+        ctx = super().render(context, instance, placeholder)
+        request = ctx.get("request")
+
+        ctx["form"] = CandidateListFilter(request.GET)
+        # Filtered List
+        ctx["object_list"] = Candidate.objects.filter(
+            status=CandidateStatusChoices.published
+        )
+
+        filter_state = request.GET.get("uf", None)
+        if filter_state:
+            ctx["object_list"] = ctx["object_list"].filter(
+                place__state__iexact=filter_state
+            )
+
+        return ctx
--- app/project/settings/cms.py
+++ app/project/settings/cms.py
@@ -12,7 +12,7 @@ CMS_TEMPLATES = [
 CMS_PLACEHOLDER_CONF = {
     "content": {
         "name": "Blocos",
-        "plugins": ["BlockPlugin", "BlockPressurePlugin"],
+        "plugins": ["BlockPlugin", "BlockPressurePlugin", "EleicaoCandidateListPlugin"],
     },
     "navigation": {
         "name": "Navegação",
