b1cb774bd821bf6966e3c4d5d646fece1d6d1a5e
joaopclima <joaopedrocostalima@gmail.com>
2023-08-21 15:25:56 -0300

    feat:add-5-grid

--- app/contrib/frontend/grid/models.py
+++ app/contrib/frontend/grid/models.py
@@ -20,7 +20,9 @@ class GridColumnChoices(models.TextChoices):
     grid_2 = "grid-cols-1 md:grid-cols-2", "2 Colunas"
     grid_3 = "grid-cols-1 md:grid-cols-3", "3 Colunas"
     grid_4 = "grid-cols-1 sm:grid-cols-2 md:grid-cols-4", "4 Colunas"
+    grid_5 = "grid-cols-1 sm:grid-cols-2 md:grid-cols-5", "5 Colunas"
     grid_1_2 = "grid-cols-1 md:grid-cols-[1fr_2fr]", "2 colunas, 4 x 8"
+    
 
 
 class Grid(CMSPlugin):
