b7d0db465f35d2afa2b48d69253fdd1d8c580ad7
Igor Santos <igr.exe@gmail.com>
2023-08-30 13:23:22 -0300

    feat(eleicao): add candidate filtered on context when voter form success

--- app/eleicao/templatetags/eleicao_candidate_tags.py
+++ app/eleicao/templatetags/eleicao_candidate_tags.py
@@ -1,9 +1,16 @@
 import re
 from django import template
 
+from ..models import Voter, Candidate
+
+
 register = template.Library()
 
 
 @register.inclusion_tag("eleicao/templatetags/candidate_list.html")
-def render_candidate_list(voter):
-    return {}
+def render_candidate_list(voter: Voter):
+    object_list = Candidate.objects.filter(place__state=voter.state, place__city=voter.city)
+
+    return {
+        "object_list": object_list
+    }
