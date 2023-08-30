7d59a6e6900526ca539f14ef6b127f6ef1ab4b64
Igor Santos <igr.exe@gmail.com>
2023-08-30 13:51:56 -0300

    feat(eleicao): add place filter to voter form

--- app/eleicao/forms/create.py
+++ app/eleicao/forms/create.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.urls import reverse_lazy
 from django_select2 import forms as s2forms
 
 
@@ -16,7 +17,7 @@ class PlacesWidget(s2forms.ModelSelect2Widget):
 class VoterForm(forms.ModelForm):
     class Meta:
         model = Voter
-        fields = ["name", "email", "whatsapp", "state", "city"]
+        fields = ["name", "email", "whatsapp", "state", "city", "place"]
         widgets = {
             "name": forms.TextInput(attrs={"placeholder": "Seu nome completo"}),
             "email": forms.EmailInput(attrs={"placeholder": "Seu email"}),
@@ -26,9 +27,22 @@ class VoterForm(forms.ModelForm):
                     "data-mask": "00 0 0000-0000",
                 }
             ),
+            "state": forms.Select(
+                attrs={
+                    "data-cep-fields": "state",
+                    "data-cep-url": reverse_lazy("eleicao:cep"),
+                }
+            ),
+            "city": forms.Select(
+                attrs={
+                    "data-cep-fields": "city",
+                    "data-cep-url": reverse_lazy("eleicao:cep"),
+                }
+            ),
+            "place": forms.Select(
+                attrs={
+                    "data-cep-fields": "place",
+                    "data-cep-url": reverse_lazy("eleicao:cep"),
+                }
+            ),
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.fields["city"].widget = forms.Select()
