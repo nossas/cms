0996b171b07d6868c664348ffe37ea314810c644
miguelzinh3 <miguelssoares@pm.me>
2023-09-04 21:40:48 -0300

    feat(eleicao): add share btns in voter form

--- app/eleicao/templatetags/eleicao_candidate_tags.py
+++ app/eleicao/templatetags/eleicao_candidate_tags.py
@@ -3,6 +3,8 @@ from django import template
 
 from ..models import Voter, Candidate
 
+from urllib import parse
+
 
 register = template.Library()
 
@@ -20,4 +22,25 @@ def render_candidate_list(voter: Voter):
     else:
         object_list = Candidate.objects.filter(place=voter.place)
 
-    return {"object_list": object_list, "is_empty": not object_list.exists()}
+    url = "https://aeleicaodoano.org"
+
+    msg_whatsapp = parse.quote(
+        f"Oie! Tá sabendo da Eleição do Ano? Sim, esse ano temos uma eleição importantíssima: os municípios brasileiros vão eleger conselheiros e conselheiras tutelares no dia 1 de outubro. É o futuro das nossas crianças e adolescentes em jogo! Não fique de fora:"
+        + "\n"
+        + url
+    )
+    msg_twitter = parse.quote(
+        f"Acabei de me comprometer a participar da Eleição do Ano, que define o presente e o futuro do nosso país no dia 1 de outubro. Vem você também defender nossas crianças e adolescentes: "
+        + url
+    )
+    url_facebook_modal = parse.quote(url + "&amp;src=sdkpreparse")
+    msg_copy_link = url
+
+    return {
+        "object_list": object_list,
+        "is_empty": not object_list.exists(),
+        "msg_whatsapp": msg_whatsapp,
+        "msg_twitter": msg_twitter,
+        "url_facebook_modal": url_facebook_modal,
+        "msg_copy_link": msg_copy_link
+    }
