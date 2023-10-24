393a7e35de3967f346b4dda0fece076a3096498c
miguelzinh3 <miguelssoares@pm.me>
2023-10-24 17:12:40 -0300

    fix: test example from geojson

--- app/contrib/frontend/maps/static/maps/js/maps.js
+++ app/contrib/frontend/maps/static/maps/js/maps.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-undef */
+/**
+ * autocomplete with geojson
+ */
+
 let config = {
   minZoom: 7,
   maxZoom: 18,
@@ -11,10 +16,8 @@ const lng = -46.63366;
 const mapWrapper = document.querySelector("#map");
 
 if (mapWrapper) {
-  // Adiciona o mapa
   const map = L.map("map", config).setView([lat, lng], zoom);
 
-  // Carrega e mostra o layer
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -36,7 +39,7 @@ if (mapWrapper) {
       container.insertAdjacentHTML(
         "beforeend",
         `<div class="auto-search-wrapper loupe">
-          <input type="text" id="show-all-values" autocomplete="off" placeholder="${searchPlaceholder}" />
+          <input type="text" id="local" autocomplete="off" placeholder="${searchPlaceholder}" />
         </div>`
       );
 
@@ -46,11 +49,10 @@ if (mapWrapper) {
 
   new L.Control.Search().addTo(map);
 
-  let startPoint, endPoint, geojsonLayer, markersGeoJSON;
   let geojsonarray = [];
 
   let popupText = (properties) => {
-    let ifObsProperty = !!properties?.observacoes ? `<span>Obs: ${properties.observacoes}</span><br>` : "";
+    let ifObsProperty = !!properties?.observacoes ? `<span><strong>Obs:</strong> ${properties.observacoes}</span><br>` : "";
 
     return `<span><strong>Número:</strong> ${properties.ln_codigo}</span><br>` +
     `<span><strong>Nome:</strong> ${properties.title}</span><br>` +
@@ -61,60 +63,9 @@ if (mapWrapper) {
     ifObsProperty;
   }
 
-  function addMarkers(coordinates, properties) {
-    let LeafIcon = L.Icon.extend({
-      options: {
-          iconSize:     [40, 40],
-          iconAnchor:   [22, 50],
-          popupAnchor:  [-3, -76]
-      }
-    });
-    
-    // Remove marcadores existentes
-    if (startPoint) map.removeLayer(startPoint);
-    if (endPoint) map.removeLayer(endPoint);
-
-    function createMarker(iconUrl, coordinates) {
-      const icon = iconUrl ? new LeafIcon({ iconUrl }) : null;
-      return L.marker([coordinates[1], coordinates[0]], { icon });
-    }
-
-    startPoint = createMarker(mapWrapper.dataset.mapsIconsPointA, coordinates[0]);
-
-    endPoint = createMarker(mapWrapper.dataset.mapsIconsPointB, coordinates[coordinates.length -1])
 
 
-    [startPoint, endPoint].forEach(marker => {
-      marker.bindPopup(popupText(properties))
-    });
-  }
-
-  function addPolyline(coordinates, properties) {
-    const lineColor = mapWrapper.dataset.mapsLinecolor;
-
-    if (geojsonLayer) map.removeLayer(geojsonLayer);
-
-    geojsonLayer = L.geoJSON({
-      type: "LineString",
-      coordinates: coordinates,
-    }, {
-      style: {
-        color: lineColor || "red",
-        weight: 3,
-        opacity: 1,
-        fillOpacity: 0.5,
-      },
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup(popupText(properties));
-      },
-    })
-  }
-
-  new Autocomplete("show-all-values", {
-    clearButton: true,
-    cache: true,
-    showAllValues: true,
-
+  new Autocomplete("local", {
     onSearch: ({ currentValue }) => {
       const api = mapWrapper.dataset.mapsGeojson;
       return new Promise((resolve) => {
@@ -143,35 +94,88 @@ if (mapWrapper) {
       });
     },
 
-    onResults: ({ matches, template }) =>
-      matches === 0 ? template : matches.map((el) => `<li><div class="title">${el.properties.ln_codigo} - ${el.properties.title}</div></li>`).join(""),
+    onResults: ({ matches, template }) => {
+      return matches === 0
+        ? template
+        : matches
+            .map((el) => {
+              return `
+                <li>
+                  <div class="title">${el.properties.ln_codigo} - ${el.properties.title}</div>
+                </li>`;
+            })
+            .join("");
+    },
 
     onSubmit: ({ object }) => {
-      console.log("maps on submit", object)
-      const coordinates = object.geometry.coordinates;
       const properties = object.properties;
+      const coordinates = object.geometry.coordinates;
+      const lineColor = mapWrapper.dataset.mapsLinecolor;
 
-      addPolyline(coordinates, properties);
+      const geojsonlayer = L.geoJSON(object, {
+        style: function (feature) {
+          return {
+            color: lineColor || "red",
+            weight: 7,
+            opacity: 1,
+            fillOpacity: 0.7,
+          };
+        },
+        onEachFeature: function (feature, layer) {
+          layer.bindPopup(popupText(properties));
+        },
+      });
 
-      map.fitBounds(geojsonLayer.getBounds(), { padding: [150, 150] });
+      const geojsonmarkers = (properties, coordinates) => {
+
+        let LeafIcon = L.Icon.extend({
+          options: {
+              iconSize:     [40, 40],
+              iconAnchor:   [22, 50],
+              popupAnchor:  [-3, -76]
+          }
+        });
+
+        if (mapWrapper.dataset.mapsIconsPointA) {
+          const startIcon = new LeafIcon({ iconUrl: mapWrapper.dataset.mapsIconsPointA });
+          startPoint = L.marker([coordinates[0][1], coordinates[0][0]], { icon: startIcon }).addTo(map);
+        } else {
+          startPoint = L.marker([coordinates[0][1], coordinates[0][0]]).addTo(map);
+        }
+        
+        if (mapWrapper.dataset.mapsIconsPointB) {
+          const endIcon = new LeafIcon({ iconUrl: mapWrapper.dataset.mapsIconsPointB }).addTo(map);
+          endPoint = L.marker([coordinates[coordinates.length - 1][1], coordinates[coordinates.length - 1][0]], { icon: endIcon }).addTo(map);
+        } else {
+          endPoint = L.marker([coordinates[coordinates.length - 1][1], coordinates[coordinates.length - 1][0]]).addTo(map);
+        }
+
+        [startPoint, endPoint].forEach(marker => {
+          marker.bindPopup(popupText(properties))
+        });
+      }
+
+      map.fitBounds(geojsonlayer.getBounds(), { padding: [150, 150] });
 
       if (geojsonarray.includes(object.properties.id)) return;
       geojsonarray.push(object.properties.id);
 
-      geojsonLayer.addTo(map);
-      // startPoint.addTo(map);
-      // endPoint.addTo(map);
+
+      geojsonmarkers(properties, coordinates);
+      geojsonlayer.addTo(map);
     },
 
-    noResults: ({ currentValue, template }) => template(`<li>Sem resultados: "${currentValue}"</li>`),
+    noResults: ({ currentValue, template }) =>
+      template(`<li>Sem resultados: "${currentValue}"</li>`),
 
     onReset: () => {
-      // Remove marcadores e linha
-      [startPoint, endPoint, geojsonLayer].forEach(layer => {
-        if (layer) map.removeLayer(layer);
+      // remove all layers
+      map.eachLayer(function (layer) {
+        if (!!layer.toGeoJSON) {
+          map.removeLayer(layer);
+        }
       });
-
       geojsonarray = [];
     },
-  }); 
+  });
 }
