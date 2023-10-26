dd9d79ecd10906e4babb1d9f9bc5a958139601f7
miguelzinh3 <miguelssoares@pm.me>
2023-10-26 18:14:09 -0300

    feat: add metadataextension for meta config

--- app/contrib/campaign/admin.py
+++ app/contrib/campaign/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from cms.extensions import PageExtensionAdmin
 
-from .models import IconExtension
+from .models import IconExtension, MetaDataExtension
 
 
 class IconExtensionAdmin(PageExtensionAdmin):
     pass
 
 admin.site.register(IconExtension, IconExtensionAdmin)
+admin.site.register(MetaDataExtension, PageExtensionAdmin)
