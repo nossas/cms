3b088a65d497ffcbd9806e133cbbf179df0b8d44
Igor Santos <igr.exe@gmail.com>
2023-12-21 15:21:03 -0300

    chore(design): add heading plugin

---
+++ app/contrib/designsystem/cms_plugins.py
@@ -0,0 +1,14 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+
+class DSCardPlugin(CMSPluginBase):
+    render_template = "ds/plugins/card.html"
+
+plugin_pool.register_plugin(DSCardPlugin)
+
+
+class DSHeadingPlugin(CMSPluginBase):
+    render_template = "ds/plugins/heading.html"
+
+plugin_pool.register_plugin(DSHeadingPlugin)
