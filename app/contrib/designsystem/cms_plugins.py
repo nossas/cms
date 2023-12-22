cfe8211aa5bbb1c8eda264d9c12454bbaf09e17c
Igor Santos <igr.exe@gmail.com>
2023-12-22 16:24:33 -0300

    feat(design): custom modal and accordion

--- app/contrib/designsystem/cms_plugins.py
+++ app/contrib/designsystem/cms_plugins.py
@@ -1,9 +1,15 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
+from djangocms_frontend.contrib.accordion.cms_plugins import (
+    AccordionPlugin,
+    AccordionItemPlugin,
+)
+
 
 class DSCardPlugin(CMSPluginBase):
     render_template = "ds/plugins/card.html"
+
 
 plugin_pool.register_plugin(DSCardPlugin)
 
@@ -11,10 +17,40 @@ plugin_pool.register_plugin(DSCardPlugin)
 class DSHeadingPlugin(CMSPluginBase):
     render_template = "ds/plugins/heading.html"
 
+
 plugin_pool.register_plugin(DSHeadingPlugin)
 
 
 class DSBreaklinePlugin(CMSPluginBase):
     render_template = "ds/plugins/breakline.html"
 
+
 plugin_pool.register_plugin(DSBreaklinePlugin)
+
+
+class DSAccordionPlugin(AccordionPlugin):
+    name = "DSAccordionPlugin"
+    render_template = "ds/plugins/accordion.html"
+    child_classes = [
+        "DSAccordionItemPlugin",
+    ]
+
+
+plugin_pool.register_plugin(DSAccordionPlugin)
+
+
+class DSAccordionItemPlugin(AccordionItemPlugin):
+    name = "DSAccordionItemPlugin"
+    render_template = "ds/plugins/accordion_item.html"
+    parent_classes = [
+        "DSAccordionPlugin",
+    ]
+
+plugin_pool.register_plugin(DSAccordionItemPlugin)
+
+
+class DSModalPlugin(CMSPluginBase):
+    render_template = "ds/plugins/modal.html"
+
+
+plugin_pool.register_plugin(DSModalPlugin)
