1447799bd8148e860ebff69eba7edd568ccf96be
Igor Santos <igr.exe@gmail.com>
2023-12-25 15:28:39 -0300

    feat(nossas): enable i18n

--- app/nossas/settings.py
+++ app/nossas/settings.py
@@ -1,5 +1,7 @@
 from project.settings import *
 
+# Apps
+
 INSTALLED_APPS += [
     # Build Bootstrap SCSS
     "compressor",
@@ -8,6 +10,9 @@ INSTALLED_APPS += [
     "nossas.design",
 ]
 
+
+# Static files
+
 STATICFILES_FINDERS += [
     #
     "compressor.finders.CompressorFinder",
@@ -15,14 +20,46 @@ STATICFILES_FINDERS += [
 
 COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 
+
+# Middlewares
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "project.middleware.WwwRedirectMiddleware",
+    "eleicao.middleware.EleicaoRedirectMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    # Add i18n middleware
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "contrib.bonde.middleware.SiteMiddleware",
+    "cms.middleware.user.CurrentUserMiddleware",
+    "cms.middleware.page.CurrentPageMiddleware",
+    "cms.middleware.toolbar.ToolbarMiddleware",
+    "cms.middleware.language.LanguageCookieMiddleware",
+]
+
+# Internationalization
+
+USE_I18N = True
+
+LANGUAGES = [
+    ("pt-br", "Português"),
+    ("en", "Inglês"),
+]
+
+
+# CMS
+
 CMS_TEMPLATES += [
     ("nossas/base.html", "NOSSAS"),
 ]
 
 CMS_PLACEHOLDER_CONF = {
     **CMS_PLACEHOLDER_CONF,
-    "main": {
-        "name": "Corpo da página",
-        "plugins": ["TextPlugin"]
-    },
+    "main": {"name": "Corpo da página", "plugins": ["TextPlugin"]},
 }
