a8f568b3c2b9570d94674a7b63b1820684530a40
miguelzinh3 <miguelssoares@pm.me>
2024-01-10 11:36:09 -0300

    feat: add css grid to styleguide template

--- app/nossas/views.py
+++ app/nossas/views.py
@@ -1,8 +1,6 @@
 from django.views.generic import TemplateView
 from .models import StyleGuideModel
 
-from cms.plugin_rendering import ContentRenderer
-
 class StyleGuideView(TemplateView):
     template_name = "styleguide.html"
     name = "StyleGuide Nossas"
