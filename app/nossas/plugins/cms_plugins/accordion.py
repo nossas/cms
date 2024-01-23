3b9a79c27557b5dac3124cbaeeea1797b618ec63
miguelzinh3 <miguelssoares@pm.me>
2024-01-23 19:44:54 -0300

    feat(nossas): refactor accordion plugin

--- app/nossas/plugins/cms_plugins/accordion.py
+++ app/nossas/plugins/cms_plugins/accordion.py
@@ -1,3 +1,5 @@
+from cms.api import add_plugin
+
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
@@ -6,8 +8,31 @@ from nossas.plugins.models.accordionmodel import Accordion
 
 @plugin_pool.register_plugin
 class AccordionPlugin(CMSPluginBase):
-    name = "Accordion"
+    name = "Acordeon"
     module = "NOSSAS"
     model = Accordion
     render_template = "nossas/plugins/accordion.html"
     allow_children = True
+
+    def add_default_text(self, obj):
+        placeholder = obj.placeholder
+        language = obj.language
+
+        plugin_type = "TextPlugin"
+        child_attrs = {
+            "body": """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim veniam, quis nostrud exercitation.</p>"""
+        }
+        obj = add_plugin(
+            placeholder=placeholder,
+            plugin_type=plugin_type,
+            language=language,
+            target=obj,
+            **child_attrs,
+        )
+
+    def save_model(self, request, obj, form, change):
+        """Change save_model to create plugins by layout"""
+        super().save_model(request, obj, form, change)
+
+        if not change:
+            self.add_default_text(obj)
--- app/nossas/plugins/models/accordionmodel.py
+++ app/nossas/plugins/models/accordionmodel.py
@@ -3,4 +3,4 @@ from cms.plugin_base import CMSPlugin
 
 
 class Accordion(CMSPlugin):
-    title = models.TextField(verbose_name="Título")
+    title = models.TextField(verbose_name="Título do Acordeon")
