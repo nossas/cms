29cb591aab58ee53348228fdab1766f09e38b960
Igor Santos <igr.exe@gmail.com>
2024-01-26 14:31:22 -0300

    fix(nossas): change urls reverse to institutional information form

--- app/nossas/apps/views/institutional.py
+++ app/nossas/apps/views/institutional.py
@@ -6,12 +6,12 @@ from ..models.institutional import InstitutionalInformation
 
 
 def redirect_add_or_change(request):
-    url = admin_reverse("institutional_institutionalinformation_add")
+    url = admin_reverse("apps_institutionalinformation_add")
 
     try:
         if request.current_site.institutionalinformation:
             url = admin_reverse(
-                "institutional_institutionalinformation_change",
+                "apps_institutionalinformation_change",
                 kwargs={"object_id": request.current_site.institutionalinformation.id},
             )
     except InstitutionalInformation.DoesNotExist:
