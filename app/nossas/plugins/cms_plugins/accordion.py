a018dee9d1d98eaef863eed584cfb62608c7554a
Igor Santos <igr.exe@gmail.com>
2024-02-08 18:24:29 -0300

    chore(nossas): add default accordion item when create accordion

--- app/nossas/plugins/cms_plugins/accordion.py
+++ app/nossas/plugins/cms_plugins/accordion.py
@@ -14,6 +14,41 @@ class AccordionPlugin(CMSPluginBase):
     allow_children = True
     child_classes = ["AccordionItemPlugin"]
 
+    def add_default_item(self, obj):
+        placeholder = obj.placeholder
+        language = obj.language
+
+        plugin_type = "AccordionItemPlugin"
+        child_attrs = {
+            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor qui adipisci velit, sed qut enim ad minima?"
+        }
+        target = add_plugin(
+            placeholder=placeholder,
+            plugin_type=plugin_type,
+            language=language,
+            target=obj,
+            **child_attrs,
+        )
+
+        plugin_type = "TextPlugin"
+        child_attrs = {
+            "body": """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim veniam, quis nostrud exercitation.</p>"""
+        }
+        add_plugin(
+            placeholder=placeholder,
+            plugin_type=plugin_type,
+            language=language,
+            target=target,
+            **child_attrs,
+        )
+
+    def save_model(self, request, obj, form, change):
+        """Change save_model to create plugins by layout"""
+        super().save_model(request, obj, form, change)
+
+        if not change:
+            self.add_default_item(obj)
+
 
 @plugin_pool.register_plugin
 class AccordionItemPlugin(CMSPluginBase):
