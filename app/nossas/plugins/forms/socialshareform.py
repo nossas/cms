e21932b5a9ca50d228df8e3d42ab1998c4267b1d
miguelzinh3 <miguelssoares@pm.me>
2024-03-04 12:35:21 -0300

    feat(nossas): add cta and share box options

--- app/nossas/plugins/forms/socialshareform.py
+++ app/nossas/plugins/forms/socialshareform.py
@@ -12,20 +12,20 @@ class SocialShareIconSelect(forms.CheckboxSelectMultiple):
 
 class SocialSharePluginForm(forms.ModelForm):
     selected_social_media = forms.MultipleChoiceField(choices=[
-        ('twitter', 'Twitter'),
-        ('facebook', 'Facebook'),
-        ('whatsapp', 'WhatsApp'),
-        ('linkedin', 'LinkedIn'),
+        ("facebook", "Facebook"),
+        ("linkedin", "LinkedIn"),
+        ("twitter", "Twitter"),
+        ("whatsapp", "WhatsApp"),
     ], widget=SocialShareIconSelect, required=False)
 
     class Meta:
         model = SocialSharePluginModel
-        fields = ['selected_social_media']
+        fields = ["selected_social_media"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance:
-            self.fields['selected_social_media'].initial = self.instance.get_selected_social_media_list()
+            self.fields["selected_social_media"].initial = self.instance.get_selected_social_media_list()
 
     def clean_selected_social_media(self):
-        return ','.join(self.cleaned_data['selected_social_media'])
+        return ",".join(self.cleaned_data["selected_social_media"])
