50a76047fb9db42aea6a870731c833bfb0231f28
Igor Santos <igr.exe@gmail.com>
2024-03-04 18:27:51 -0300

    fix(build): add default ALLOWED_HOSTS to static cli

--- app/project/settings/base.py
+++ app/project/settings/base.py
@@ -42,7 +42,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
