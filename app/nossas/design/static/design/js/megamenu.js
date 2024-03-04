ac8158f6eddfede28ed9de7feb7d9a497718a72c
Igor Santos <igr.exe@gmail.com>
2024-03-04 13:15:58 -0300

    fix(nossas): add behavior with hover menu

--- app/nossas/design/static/design/js/megamenu.js
+++ app/nossas/design/static/design/js/megamenu.js
@@ -16,5 +16,25 @@
         $('[data-bs-toggle="dropdown"]').on('hide.bs.dropdown', (evt) => {
             $(evt.target).next().removeAttr("style");
         });
+
+        // Open Dropdown on Navbar with hover
+        $('.d-none .navbar-nav > .child').hover(function() {
+            const dropdownBtn = $(this).find('.dropdown-toggle');
+            const dropdownEl = new bootstrap.Dropdown(dropdownBtn);
+            dropdownEl.show();
+            
+          }, function() {
+            const dropdownBtn = $(this).find('.dropdown-toggle');
+            const dropdownEl = new bootstrap.Dropdown(dropdownBtn);
+            dropdownEl.hide();
+        });
+
+        // Enable click on Dropdown
+        $('.d-none a.dropdown-toggle').on("click", function() {
+            // $(this).attr.href
+            
+            console.log("clicked", $(this).attr('href'));
+            window.location.href = $(this).attr('href')
+        });
     });
 }(window.jQuery));
