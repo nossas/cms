ffb2864965388b78a80fb0a939204949acfdccda
Igor Santos <igr.exe@gmail.com>
2024-03-21 17:38:02 -0300

    feat(nossas): add background size option to edit full slider page

--- app/nossas/plugins/models/slidermodel.py
+++ app/nossas/plugins/models/slidermodel.py
@@ -8,8 +8,17 @@ from nossas.design.models import (
 )
 
 
+class BackgroundSize(models.TextChoices):
+    contain = "contain"
+    cover = "cover"
+    initial = "initial"
+
+
 class FullPageSlider(UIBackgroundMixin, UICMSPlugin):
     background_image = FilerImageField(on_delete=models.SET_NULL, blank=True, null=True)
     x_and_y_center = models.BooleanField(
         verbose_name=_("Alinhar todo o conteúdo ao centro"), default=False
+    )
+    background_size = models.CharField(
+        choices=BackgroundSize.choices, max_length=8, default=BackgroundSize.contain
     )
