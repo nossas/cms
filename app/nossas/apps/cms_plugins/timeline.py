5324fdee9fe2ccb856b61dff1600517e51e977df
miguelzinh3 <miguelssoares@pm.me>
2024-03-25 17:35:53 -0300

    feat(nossas): add order by unique year-month func in timeline plugin

--- app/nossas/apps/cms_plugins/timeline.py
+++ app/nossas/apps/cms_plugins/timeline.py
@@ -1,3 +1,6 @@
+from itertools import zip_longest
+from collections import defaultdict
+
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
@@ -10,15 +13,54 @@ class TimelinePlugin(CMSPluginBase):
     module = "NOSSAS"
     render_template = "nossas/timeline/plugins/timeline_plugin.html"
 
+    def prepare_timeline_events(self, events_world, events_nossas):
+        # Organiza eventos por ano e mês
+        events_by_year_month_world = defaultdict(lambda: defaultdict(list))
+        events_by_year_month_nossas = defaultdict(lambda: defaultdict(list))
+        
+        for event in events_world:
+            events_by_year_month_world[event.year][event.month].append(event)
+        
+        for event in events_nossas:
+            events_by_year_month_nossas[event.year][event.month].append(event)
+
+        unique_years_months = set(events_by_year_month_world.keys()) | set(events_by_year_month_nossas.keys())
+        for year in unique_years_months:
+            months_world = set(events_by_year_month_world[year].keys())
+            months_nossas = set(events_by_year_month_nossas[year].keys())
+            all_months = months_world | months_nossas
+            
+            for month in sorted(all_months):
+                if month not in months_world:
+                    events_by_year_month_world[year][month] = []
+                if month not in months_nossas:
+                    events_by_year_month_nossas[year][month] = []
+
+        sorted_events_world = sorted(
+            [(year, month, events_by_year_month_world[year][month]) for year in events_by_year_month_world for month in sorted(events_by_year_month_world[year])],
+            key=lambda x: (x[0], x[1])
+        )
+        sorted_events_nossas = sorted(
+            [(year, month, events_by_year_month_nossas[year][month]) for year in events_by_year_month_nossas for month in sorted(events_by_year_month_nossas[year])],
+            key=lambda x: (x[0], x[1])
+        )
+
+        # Mantem os eventos alinhados entre as categorias
+        aligned_events_world, aligned_events_nossas = zip(*zip_longest(sorted_events_world, sorted_events_nossas, fillvalue=(None, None, [])))
+
+        return aligned_events_world, aligned_events_nossas
+
     def render(self, context, instance, placeholder):
         context = super().render(context, instance, placeholder)
 
-        events_world = TimelineEvent.on_site.filter(event_context="mundo")
-        events_nossas = TimelineEvent.on_site.filter(event_context="nossas")
+        events_world = TimelineEvent.on_site.filter(event_context="mundo").order_by('year', 'month', 'day')
+        events_nossas = TimelineEvent.on_site.filter(event_context="nossas").order_by('year', 'month', 'day')
+
+        aligned_events_world, aligned_events_nossas = self.prepare_timeline_events(events_world, events_nossas)
 
         context.update({
-            "events_world": events_world,
-            "events_nossas": events_nossas
+            'aligned_events_world': aligned_events_world,
+            'aligned_events_nossas': aligned_events_nossas,
         })
 
         return context
