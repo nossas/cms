4acdd9ec9352096705cbd70f2ae67a1fa50318b5
Igor Santos <igr.exe@gmail.com>
2024-04-17 16:36:23 -0300

    feat(ds): add carousel base plugin

---
+++ app/contrib/ds/carousel/cms_plugins.py
@@ -0,0 +1,22 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+
+@plugin_pool.register_plugin
+class CarouselPlugin(CMSPluginBase):
+    name = "Carrossel"
+    render_template = "carousel/plugins/carousel.html"
+    allow_children = True
+    child_classes = ["CarouselContentPlugin"]
+
+
+@plugin_pool.register_plugin
+class CarouselContentPlugin(CMSPluginBase):
+    name = "Carrosel Item"
+    render_template = "carousel/plugins/carousel_content.html"
+    allow_children = True
+    parent_classes = ["CarouselPlugin"]
+
+    def render(self, context, instance, placeholder):
+        context["is_active"] = instance.position == 0
+        return super().render(context, instance, placeholder)
