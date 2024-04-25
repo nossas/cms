db79515c75bb80e76dca2ae5d237783bdd0ce33c
miguelzinh3 <miguelssoares@pm.me>
2024-04-25 17:47:01 -0300

    fix(nossas): refactor headline plugin

--- app/nossas/plugins/models/headlinemodel.py
+++ app/nossas/plugins/models/headlinemodel.py
@@ -12,13 +12,9 @@ class HeadlineIconChoices(models.TextChoices):
 
 
 class Headline(CMSPlugin):
-    title = models.CharField(verbose_name="Título", max_length=180)
     icon = models.CharField(
         choices=HeadlineIconChoices.choices, max_length=120, blank=True, null=True
     )
-
-    def __str__(self):
-        return self.title
 
     @property
     def get_full_path_icon(self):
