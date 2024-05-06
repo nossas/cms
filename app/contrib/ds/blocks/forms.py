6469c1bdd3d79705490aabc4b42ddf7f71cf48e7
Igor Santos <igr.exe@gmail.com>
2024-05-06 09:58:40 -0300

    fix(ds): add layouts icon to template block

--- app/contrib/ds/blocks/forms.py
+++ app/contrib/ds/blocks/forms.py
@@ -120,17 +120,18 @@ class BlockForm(
 
 
 class BlockTemplate(models.TextChoices):
-    empty = "", "empty"
-    content_one_col = "content_one_col", "Conteúdo 1 coluna"
-    content_three_cols = "content_three_cols", "Conteúdo 3 colunas"
-    content_two_cols_6x6 = "content_two_cols_6x6", "Conteúdo 2 colunas (6x6)"
-    content_two_cols_4x8 = "content_two_cols_4x8", "Conteúdo 2 colunas (4x8)"
-    content_four_cols = "content_four_cols", "Conteúdo 4 colunas"
+    empty = "", "Vazio"
+    content_one_col = "content_one_col", "1 coluna"
+    content_three_cols = "content_three_cols", "3 colunas"
+    content_two_cols_6x6 = "content_two_cols_6x6", "2 colunas (6x6)"
+    content_two_cols_4x8 = "content_two_cols_4x8", "2 colunas (4x8)"
+    content_four_cols = "content_four_cols", "4 colunas"
 
 
 class BlockTemplateForm(BlockForm):
     template = forms.ChoiceField(
-        choices=BlockTemplate.choices, initial=BlockTemplate.empty, required=False
+        choices=BlockTemplate.choices, initial=BlockTemplate.empty, required=False,
+        help_text=_("Selecione um tipo de bloco para adicionar à página")
     )
 
     class Meta:
