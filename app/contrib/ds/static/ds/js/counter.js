e90cb4f1863a8e64ba1f28c8838574088ced5b01
miguelzinh3 <miguelssoares@pm.me>
2024-05-13 16:47:01 -0300

    feat(ds): add days left and countdown js funciotns

--- app/contrib/ds/static/ds/js/counter.js
+++ app/contrib/ds/static/ds/js/counter.js
@@ -1,12 +1,15 @@
 import { CountUp } from './lib/countUp.min.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-  function activateCountUp(selector) {
-    const element = document.querySelector(selector);
-    const startNum = parseInt(element.getAttribute('data-start-num'), 10);
-    const endNum = parseInt(element.getAttribute('data-end-num'), 10);
+  const counterNumber = document.getElementById("counterNumber");
+  const daysLeftElement = document.getElementById("counterDate");
 
-    const countUp = new CountUp(element, endNum, {
+  // Animação numérica
+  if (counterNumber.dataset.startNum || counterNumber.dataset.endNum) {
+    const startNum = parseInt(counterNumber.dataset.startNum, 10);
+    const endNum = parseInt(counterNumber.dataset.endNum, 10);
+
+    const countUp = new CountUp(counterNumber, endNum, {
       startVal: startNum,
       decimalPlaces: 0,
       duration: 2,
@@ -20,5 +23,24 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  activateCountUp('#counter');
+  // Contagem regressiva de dias
+  if (daysLeftElement.dataset.startDate && daysLeftElement.dataset.endDate) {
+    const startDate = new Date(daysLeftElement.dataset.startDate);
+    const endDate = new Date(daysLeftElement.dataset.endDate);
+    const diffTime = Math.abs(endDate - startDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    const countUp = new CountUp(daysLeftElement, diffDays, {
+      startVal: 0,
+      decimalPlaces: 0,
+      duration: 2,
+      useEasing: true,
+    });
+
+    if (!countUp.error) {
+      countUp.start();
+    } else {
+      console.error(countUp.error);
+    }
+  }
 });
