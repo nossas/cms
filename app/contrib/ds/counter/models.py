6eaf6bc12a48229ebd1202a34dabb83c9571e864
miguelzinh3 <miguelssoares@pm.me>
2024-05-17 14:52:52 -0300

    feat(adp): remove fields from counter models

--- app/contrib/ds/counter/models.py
+++ app/contrib/ds/counter/models.py
@@ -9,7 +9,3 @@ class Counter(CMSPlugin):
 
     initial_date = models.DateField(verbose_name="Data de Início", null=True, blank=True)
     target_date = models.DateField(verbose_name="Data Final", null=True, blank=True)
-    
-    measurement = models.CharField(verbose_name="Unidade de medida", max_length=140, null=True, blank=True)
-    description = models.TextField(verbose_name="Descrição", null=True, blank=True)
-    tooltip_text = models.TextField(verbose_name="Texto do Tooltip", null=True, blank=True)
