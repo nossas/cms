c705bf5e1cc350c6ce79a72f09fe202edb47a4d1
Igor Santos <igr.exe@gmail.com>
2024-05-22 15:42:35 -0300

    fix(ds): direction to tooltip plugin

--- app/contrib/ds/tooltip/models.py
+++ app/contrib/ds/tooltip/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from cms.models import CMSPlugin
 
 
+class TooltipDirection(models.TextChoices):
+    left = "", _("Esquerda")
+    right = "right", _("Direita")
+
+
 class Tooltip(CMSPlugin):
-    direction = models.CharField(max_length=30, null=True, blank=True)
-    message = models.TextField()
+    direction = models.CharField(
+        verbose_name=_("Direção"),
+        max_length=30,
+        null=True,
+        blank=True,
+        choices=TooltipDirection.choices,
+    )
+    message = models.TextField(verbose_name=_("Mensagem"))
