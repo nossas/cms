d9680125fc64e1d1ef5d98845aa34c15e0b6cf4d
Mario Sergio <mariopekenossalro@gmail.com>
2024-05-28 15:38:33 -0300

    feat(ds) removed unnecessary import

--- app/contrib/ds/link/models.py
+++ app/contrib/ds/link/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.core.exceptions import ValidationError
 
 from cms.models import CMSPlugin, Page
 from contrib.ds.bs_icons import ICONS_CHOICES
