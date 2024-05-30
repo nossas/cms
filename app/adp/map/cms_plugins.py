0f5ace27a3476333669e3e11d78f7ff79569300e
Igor Santos <igr.exe@gmail.com>
2024-05-29 21:08:14 -0300

    feat(adp): add map styles flex and remove unused elements from page

--- app/adp/map/cms_plugins.py
+++ app/adp/map/cms_plugins.py
@@ -10,13 +10,3 @@ class MapPluginPublisher(CMSPluginBase):
     name = _("Mapa")
     render_template = "map/map.html"
     form = MapPluginForm
-
-    def render(self, context, instance, placeholder):
-        css_styles = []
-
-        css_styles.append("height:100vh")
-        css_styles.append("overflow:hidden")
-
-        context["css_styles"] = ";".join(css_styles)
-
-        return super().render(context, instance, placeholder)
