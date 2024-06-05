5a21a1d08e6c614d190a115c0210bac13f7d9e5f
Igor Santos <igr.exe@gmail.com>
2024-06-05 11:40:18 -0300

    feat(ds): add metadata seo in site settings

--- app/contrib/ds/admin.py
+++ app/contrib/ds/admin.py
@@ -3,15 +3,25 @@ from django.contrib.sites.models import Site
 
 from contrib.ga.admin import GAInline
 
-from .models import Theme
+from .models import Theme, SEO
 
 
 class ThemeInline(admin.StackedInline):
     model = Theme
     can_delete = False
 
+
+class SEOInline(admin.StackedInline):
+    model = SEO
+    can_delete = False
+
+
 class SiteThemeAdmin(admin.ModelAdmin):
-    inlines = (ThemeInline, GAInline, )
+    inlines = (
+        SEOInline,
+        ThemeInline,
+        GAInline,
+    )
 
 
 admin.site.unregister(Site)
