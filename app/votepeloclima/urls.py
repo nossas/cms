3978758721ca84a760ee9b42ca0ba1a179c40cbc
Igor Santos <igr.exe@gmail.com>
2024-07-01 11:15:37 -0300

    feat(vote): initial setup votepeloclime site

---
+++ app/votepeloclima/urls.py
@@ -0,0 +1,38 @@
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+
+
+urlpatterns = [
+    # path("monitoring/", include("django_prometheus.urls")),
+    path("admin/", admin.site.urls),
+    path("select2/", include("django_select2.urls")),
+    path("", include("cms.urls")),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+handler404 = "contrib.frontend.views.error_404"
+handler500 = "contrib.frontend.views.error_500"
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
