7612feee65948bd390a17f310dde364fdd0ad37b
Igor Santos <igr.exe@gmail.com>
2024-07-16 17:38:19 -0400

    fix(nossas): remove redundancy to urls

--- app/org_nossas/nossas/urls.py
+++ app/org_nossas/nossas/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-from .views import StyleGuideView
 
-from project.urls import urlpatterns
+from .views import StyleGuideView
 
 
 urlpatterns = [
@@ -13,5 +16,20 @@ urlpatterns = [
             namespace="institutional",
         ),
     ),
-    path("styleguide/", StyleGuideView.as_view())
-] + urlpatterns
+    path("styleguide/", StyleGuideView.as_view()),
+    path("monitoring/", include("django_prometheus.urls")),
+    path("admin/", admin.site.urls),
+    path("select2/", include("django_select2.urls")),
+    path("actions/", include("contrib.actions.pressure.urls")),
+    path("", include("cms.urls")),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+handler404 = "contrib.frontend.views.error_404"
+handler500 = "contrib.frontend.views.error_500"
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
