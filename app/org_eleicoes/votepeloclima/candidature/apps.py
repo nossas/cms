f1d399fa3007b4e7e4cb986bbc9e95f2fbd0c3ca
Igor Santos <igr.exe@gmail.com>
2024-07-18 12:16:18 -0400

    feat(vote): add candidature admin pages

--- app/org_eleicoes/votepeloclima/candidature/apps.py
+++ app/org_eleicoes/votepeloclima/candidature/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EleicaoConfig(AppConfig):
+class CandidatureConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'org_eleicoes.eleicaodoano.eleicao'
+    name = 'org_eleicoes.votepeloclima.candidature'
