18a431fd7940864cfc2ac51dba6a6db93c30fe4c
miguelzinh3 <miguelssoares@pm.me>
2024-07-19 16:09:59 -0300

    feat(vote): add validations to max size and format file

--- app/org_eleicoes/votepeloclima/candidature/forms.py
+++ app/org_eleicoes/votepeloclima/candidature/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.template.defaultfilters import filesizeformat
+from django.utils.translation import gettext_lazy as _
+
 from captcha.widgets import ReCaptchaV2Checkbox
 
 from .fields import ValidateOnceReCaptchaField, StateCepField, CityCepField
@@ -22,74 +25,104 @@ class CaptchaForm(forms.Form):
 
 
 class InitialForm(DisabledMixin, forms.Form):
-    legal_name = forms.CharField(label="Nome")
-    ballot_name = forms.CharField(label="Nome na urna")
-    birth_date = forms.DateField(label="Data de nascimento")
-    email = forms.EmailField(label="E-mail")
-    cpf_cnpj = forms.CharField(label="CPF/CNPJ")
-    tse_id = forms.CharField(label="Identificação TSE (?)", required=False)
+    legal_name = forms.CharField(label=_("Nome"))
+    ballot_name = forms.CharField(label=_("Nome na urna"))
+    birth_date = forms.DateField(label=_("Data de nascimento"))
+    email = forms.EmailField(label=_("E-mail"))
+    cpf_cnpj = forms.CharField(label=_("CPF/CNPJ"))
+    tse_id = forms.CharField(label=_("Identificação TSE (?)"), required=False)
 
     class Meta:
-        title = "Informações iniciais"
+        title = _("Informações iniciais")
 
 
 class ApplicationForm(DisabledMixin, forms.Form):
-    number_id = forms.IntegerField(label="Número de identificação", min_value=1)
-    intended_position = forms.CharField(label="Cargo pretendido")
-    state = StateCepField(label="Estado")
-    city = CityCepField(label="Cidade")
+    number_id = forms.IntegerField(label=_("Número de identificação"), min_value=1)
+    intended_position = forms.CharField(label=_("Cargo pretendido"))
+    state = StateCepField(label=_("Estado"))
+    city = CityCepField(label=_("Cidade"))
     is_collective_mandate = forms.BooleanField(
-        label="É um mandato coletivo?", required=False
+        label=_("É um mandato coletivo?"), required=False
     )
-    political_party = forms.CharField(label="Partido político")
+    political_party = forms.CharField(label=_("Partido político"))
     
     class Meta:
-        title = "Informações de candidatura"
+        title = _("Informações de candidatura")
 
 
 class ProfileForm(DisabledMixin, forms.Form):
-    video = forms.FileField(label="Vídeo", required=False)
-    photo = forms.FileField(label="Foto", required=False)
-    gender = forms.CharField(label="Gênero")
-    color = forms.CharField(label="Raça")
-    sexuality = forms.CharField(label="Sexualidade", required=False)
+    video = forms.FileField(label=_("Vídeo"), required=False)
+    photo = forms.FileField(label=_("Foto"), required=False)
+    gender = forms.CharField(label=_("Gênero"))
+    color = forms.CharField(label=_("Raça"))
+    sexuality = forms.CharField(label=_("Sexualidade"), required=False)
 
     class Meta:
-        title = "Complemente seu perfil"
+        title = _("Complemente seu perfil")
+
+
+    def clean_video(self):
+        content = self.cleaned_data["video"]
+        # 50MB
+        max_size = 52428800
+        if content:
+            if "video" in content.content_type:
+                if content.size > max_size:
+                    raise forms.ValidationError(
+                        _(
+                            "Por favor, escolha um video com tamanho de até %s. Tamanho Atual %s"
+                        )
+                        % (filesizeformat(max_size), filesizeformat(content.size))
+                    )
+            else:
+                raise forms.ValidationError(_("Tipo de arquivo não suportado."))
+        return content
+    
+    def clean_photo(self):
+        content = self.cleaned_data["photo"]
+        if content:
+            if content.content_type not in ["image/png", "image/jpg", "image/jpeg"]:
+                raise forms.ValidationError(
+                    _(
+                        "Somente são aceitos arquivos em PNG ou JPEG. Selecione outra imagem, por favor."
+                    )
+                )
+
+        return content
 
 
 class TrackForm(DisabledMixin, forms.Form):
-    education = forms.CharField(label="Escolaridade", required=False)
-    employment = forms.CharField(label="Ocupação", required=False)
-    short_description = forms.CharField(label="Minibio", widget=forms.Textarea())
+    education = forms.CharField(label=_("Escolaridade"), required=False)
+    employment = forms.CharField(label=_("Ocupação"), required=False)
+    short_description = forms.CharField(label=_("Minibio"), widget=forms.Textarea())
 
     class Meta:
-        title = "Sobre sua trajetória"
+        title = _("Sobre sua trajetória")
 
 
 class FlagForm(DisabledMixin, forms.Form):
-    is_renewable_energy = forms.BooleanField(label="Energia Renovável", required=False)
+    is_renewable_energy = forms.BooleanField(label=_("Energia Renovável"), required=False)
     is_transport_and_mobility = forms.BooleanField(
-        label="Transporte e Mobilidade", required=False
+        label=_("Transporte e Mobilidade"), required=False
     )
 
     class Meta:
-        title = "Bandeiras da sua candidatura"
+        title = _("Bandeiras da sua candidatura")
 
 
 class AppointmentForm(DisabledMixin, forms.Form):
-    appointment_1 = forms.BooleanField(label="Compromisso 1", required=False)
-    appointment_2 = forms.BooleanField(label="Compromisso 2", required=False)
+    appointment_1 = forms.BooleanField(label=_("Compromisso 1"), required=False)
+    appointment_2 = forms.BooleanField(label=_("Compromisso 2"), required=False)
 
     class Meta:
-        title = "Você assume compromisso com..."
+        title = _("Você assume compromisso com...")
 
 
 class CheckoutForm(forms.Form):
     is_valid = forms.BooleanField()
 
     class Meta:
-        title = "Para finalizar, confirme as suas informações"
+        title = _("Para finalizar, confirme as suas informações")
 
 
 register_form_list = [
@@ -100,5 +133,5 @@ register_form_list = [
     ("complemente-seu-perfil", ProfileForm),
     ("sobre-sua-trajetoria", TrackForm),
     ("bandeiras-da-sua-candidatura", FlagForm),
-    # ("checkout", CheckoutForm),
+    ("checkout", CheckoutForm),
 ]
