04756b299788b66bf7e0ce543240a2a7063adf65
Igor Santos <igr.exe@gmail.com>
2024-07-19 11:07:21 -0400

    feat(vote): add selected area to checkbox text field

--- app/org_eleicoes/votepeloclima/candidature/static/js/checkbox-text-widget.js
+++ app/org_eleicoes/votepeloclima/candidature/static/js/checkbox-text-widget.js
@@ -1,21 +1,37 @@
 (function ($) {
     "use strict";
     $(function () {
+        const $dropzone = $("#dropzone");
+        const $selectzone = $("#selectzone");
         // Init
         $("[data-checktext]").each((index, element) => {
             $(element).parent().prev().remove();
+
             if (!$(element).attr("checked")) {
                 $(element).parent().next().hide();
+            } else if ($dropzone.length) {
+                const $field = $(element).parent().parent().parent();
+                $field.appendTo($dropzone);
             }
         });
         // Event
         $("[data-checktext]").change((evt) => {
             if ($(evt.target).is(":checked")) {
+                if ($dropzone.length) {
+                    const $field = $(evt.target).parent().parent().parent();
+                    $field.appendTo($dropzone);
+                }
                 $(evt.target).attr("checked", "checked");
-                $(evt.target).parent().next().show();
+                const $item = $(evt.target).parent().next();
+                $item.show()
             } else {
+                if ($selectzone.length) {
+                    const $field = $(evt.target).parent().parent().parent();
+                    $field.appendTo($selectzone);
+                }
                 $(evt.target).removeAttr("checked");
-                $(evt.target).parent().next().hide();
+                const $item = $(evt.target).parent().next();
+                $item.hide();
             }
         });
     });
