48f5c13344c1b915fcc07b1b247694fc9ba8f8ab
Mario Sergio <mariopekenossalro@gmail.com>
2024-08-05 15:01:38 -0300

    feat(votepelocima): created templatetags to social media icons

---
+++ app/org_eleicoes/votepeloclima/candidature/templatetags/social_media_tags.py
@@ -0,0 +1,46 @@
+import re
+from django import template
+
+register = template.Library()
+
+pattern = re.compile(
+    r"(?:https?:\/\/)?(?:www\.)?(?:x|medium|facebook|linkedin\.com\/in|vimeo|instagram)(?:\.com\/)?(?:in\/)?@?([a-zA-Z0-9-_.]+)"
+)
+
+def url_parse(value: str, social_media_type):
+    """Parse social media URL to standardize"""
+    urls = {
+        "x": "https://x.com/",
+        "instagram": "https://instagram.com/",
+        "facebook": "https://facebook.com/",
+        "linkedin": "https://linkedin.com/in/",
+    }
+    base_url = urls.get(social_media_type, None)
+
+    result = re.search(pattern, value)
+
+    if result:
+        result = result.group(1)
+
+    if not result or not base_url:
+        return value
+
+    if result and base_url:
+        return f"{base_url}{result}"
+
+    return value
+
+
+@register.inclusion_tag("candidature/templatetags/social_media.html")
+def render_social_media(url):
+    if url and "instagram" in url:
+        return {"url": url_parse(url, "instagram"), "kind": "instagram"}
+    elif url and "facebook" in url:
+        return {"url": url_parse(url, "facebook"), "kind": "facebook"}
+    elif url and "linkedin" in url:
+        return {"url": url_parse(url, "linkedin"), "kind": "linkedin"}
+    elif url and "x" in url:
+        return {"url": url_parse(url, "x"), "kind": "x"}
+    
+
+    return {}
