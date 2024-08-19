0d0af760cf3f4bc2e2aad8a3ce1a4798f2f0bacc
Mario Sergio <mariopekenossalro@gmail.com>
2024-08-19 15:55:05 -0300

    fix(votepeloclima): fixed render city eand state names on template

--- app/org_eleicoes/votepeloclima/candidature/models.py
+++ app/org_eleicoes/votepeloclima/candidature/models.py
@@ -4,6 +4,7 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.utils.text import slugify
 
 from .choices import CandidatureFlowStatus, IntendedPosition, PoliticalParty, Gender, Color, Sexuality, Education
+from .locations_utils import get_choices, get_states
 
 
 # Acompanhar validação da candidatura
@@ -49,6 +50,14 @@ class Candidature(models.Model):
             return self.candidatureflow.get_status_display
 
         return CandidatureFlowStatus.draft
+    
+    def get_state_name(self):
+        states = dict(get_states())
+        return states.get(self.state, "")
+
+    def get_city_name(self):
+        cities = dict(get_choices(self.state))
+        return cities.get(self.city, "")
     
     def save(self, *args, **kwargs):
         if not self.slug:
