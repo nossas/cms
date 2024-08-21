c2adb1333d428ec4dab66192f84e2fc35427c9bc
Igor Santos <igr.exe@gmail.com>
2024-08-20 22:48:32 -0300

    feat(vote): add status and update styles dashboard

--- app/org_eleicoes/votepeloclima/candidature/choices.py
+++ app/org_eleicoes/votepeloclima/candidature/choices.py
@@ -5,13 +5,13 @@ class CandidatureFlowStatus(models.TextChoices):
     # Editando
     draft = "draft", "Cadastro Incompleto"
     # Já enviou um formulário, mas solicitou alterar algumas informações
-    editing = "editing", "Em edição"
+    editing = "editing", "Edição Incompleta"
     # Passou pelo checkout e submeteu a candidatura
-    submitted = "submitted", "Enviado"
+    submitted = "submitted", "Perfil em análise"
     # Não passou nas validações
-    invalid = "invalid", "Inválido"
+    invalid = "invalid", "Perfil Reprovado"
     # Passour nas validções
-    is_valid = "is_valid", "Válido"
+    is_valid = "is_valid", "Perfil Ativo"
     # # Começou a editar, seu perfil continua público, mas o processo de atualizar
     # # a candidatura vai acontecer novamente para os novos dados
     # draft_requested = "draft_requested", "Edição Requisitada"
