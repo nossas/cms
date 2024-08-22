8e1f3bca78e058bbf339804c1ae31ae6e4e7a380
Igor Santos <igr.exe@gmail.com>
2024-08-22 14:45:44 -0300

    fix(vote): add staticfiles to upload s3

--- app/org_eleicoes/votepeloclima/settings/production.py
+++ app/org_eleicoes/votepeloclima/settings/production.py
@@ -1,25 +1,42 @@
 from .base import *
 
+DEBUG = False
+
+ADMINS = env.list("ADMINS", default=[("Admin", "admin@localhost")])
+
 INSTALLED_APPS += [
     "storages",
 ]
 
 # Storages
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 
-AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default='votepeloclima')
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default="votepeloclima")
 
-AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL', default=None)
+AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL", default=None)
 
-AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='us-east-1')
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default="us-east-1")
+
+#
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+PUBLIC_STATIC_LOCATION = "static"
+
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_STATIC_LOCATION}/"
+
+STATICFILES_STORAGE = 'project.storages.PublicStaticStorage'
+
+COMPRESS_STORAGE = STATICFILES_STORAGE
+
+COMPRESS_URL = STATIC_URL
+
+COMPRESS_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_STATIC_LOCATION}/"
 
 PUBLIC_MEDIA_LOCATION = "media"
 
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{PUBLIC_MEDIA_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
 
 DEFAULT_FILE_STORAGE = "project.storages.PublicMediaStorage"
