e94dc7dc1e3e53a8415d796bff60e506ab48f3fa
Igor Santos <igr.exe@gmail.com>
2024-08-22 13:48:01 -0300

    fix(vote): config media storage on production

--- app/org_eleicoes/votepeloclima/settings/production.py
+++ app/org_eleicoes/votepeloclima/settings/production.py
@@ -18,21 +18,8 @@ AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL', default=None)
 
 AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='us-east-1')
 
-# Compress
-# 71e433d07edd
-
-# AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-
 PUBLIC_MEDIA_LOCATION = "media"
 
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{PUBLIC_MEDIA_LOCATION}/"
 
-# DEFAULT_FILE_STORAGE = "project.storages.PublicMediaStorage"
-
-# COMPRESS_OFFLINE = True
-
-# LIBSASS_OUTPUT_STYLE = 'compressed'
-
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
-# COMPRESS_ROOT = STATIC_ROOT
+DEFAULT_FILE_STORAGE = "project.storages.PublicMediaStorage"
