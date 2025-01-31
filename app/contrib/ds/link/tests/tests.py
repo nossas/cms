9eaf1db8334011797b4f23570ba65cec1d28f768
Mario Sergio <mariopekenossalro@gmail.com>
2025-01-31 12:30:35 -0300

    feat(github-actions): fixed __init__.py file name

--- app/contrib/ds/link/tests/tests.py
+++ app/contrib/ds/link/tests/tests.py
@@ -4,7 +4,7 @@ from cms.api import add_plugin, create_page
 from cms.plugin_rendering import ContentRenderer
 from cms.test_utils.testcases import CMSTestCase
 
-# from .cms_plugins import GridPlugin, ColumnPlugin
+# from ..cms_plugins import GridPlugin, ColumnPlugin
 from ..models import Context, Target, Styled, Size, IconPosition
 
 
